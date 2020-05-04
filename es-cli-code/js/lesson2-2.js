f69c223cb32d8b8c461718d2843357dfbecb1bfc
xiaoguiking <king1129371217@163.com>
2020-05-04 17:46:32 +0800

    ES5Array

---
+++ es-cli-code/js/lesson2-2.js
@@ -0,0 +1,54 @@
+// Array ES5数组遍历
+
+//  1.for
+const arr = [1, 2, 3, 4, 5];
+for (let index = 0; index < arr.length; index++) {
+  console.log(arr[index]);
+}
+
+// 2.forEach 不支持break continue
+arr.forEach(function (item, index) {
+  console.log(item, "item");
+});
+
+// 3.every
+arr.every(function (item, index) {
+  if (item === 2) {
+  } else {
+    console.log(item);
+  }
+
+  return true // 如果没有return true 返回1
+})
+
+// 4.for in遍历对象，遍历数组有问题， 可以continue
+// for in 问题
+arr.a = 8 // a 8 索引有问题
+for (let key in arr) {
+  console.log(key, arr[key])
+}
+// 0 1
+// 1 2
+// 2 3
+// 3 4
+// 4 5
+// a 8
+
+// ES6 for--of
+for (let item of arr) {
+  console.log(item)
+}
+
+// 案例
+const Pr = {
+  A: [1, 2, 3],
+  B: [4, 5, 6],
+  C: [7, 8, 9]
+}
+
+for (let index in Pr) {
+  console.log(index, Pr[index])
+}
+// A (3)[1, 2, 3]
+// B (3)[4, 5, 6]
+// C (3)[7, 8, 9]
---
+++ es-cli-code/static/lesson2-2.js
@@ -0,0 +1,27 @@
+// let块级作用域
+// {
+//   let a = 1
+//   console.log(a) // 1
+// }
+// // console.log(a) // a is not defined
+
+// var b = 3
+// let c = 4
+// console.log(b, c) // 3, 4
+
+// console.log(window.b, window.c) // 3, undefined
+
+// // var可以重新赋值 let不可以, let 不会变量提升
+// var b = 4
+// console.log(b) // 4
+
+// 1.const 不能重复赋值
+// const a = 3
+// a = 4
+// console.log(a) //A ssignment to constant variable 报错
+
+// 2.const不允许先声明在赋值
+// const x
+// x = 3
+console.log(a) // Cannot access 'a' before initialization
+let a = 1
