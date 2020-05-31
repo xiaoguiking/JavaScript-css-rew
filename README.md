65fe9654b8091abda3854fde6da91062fb1aadbb
xiaoguiking <king1129371217@163.com>
2020-05-31 16:28:15 +0800

    本地化存储

--- README.md
+++ README.md
@@ -3,4 +3,21 @@
 - CommentApp 评论功能整体组件
 - CommentInput 负责用户输入可操作的输入区域，包括输入评论的用户名、评论内容和发布按钮
 - CommentList 评论列表
-- Comment 每个评论列表由Comment 负责显示
+- Comment 每个评论列表由 Comment 负责显示
+
+### 功能需求第二阶段
+
+- 页面加载完成自动聚焦评论输入框
+- 用户名持久化存放到浏览器的 localStorage 中，页面加载时会把用户名加载出来显示到输入框，
+- 已经发布的评论持久化，存到到 localStorage 中，显示到页面的评论列表上
+- 评论显示发布日期， 如“1 秒前”， ’30 分钟前‘
+- 评论可以被删除
+- 类似 markdown 效果行内块显示，用户输入的``被处理成<code>包裹
+
+> 备注：组件的私有方法都用\_开头，所有事件监听的方法都用 handle 开头，把事件监听方法传给组件的时候属性名用 on 开头
+
+```react
+<ComponentInput
+    onSubmit={this.handleSubmitComment.bind(this)}
+>
+```
--- React/hoc-code/src/pages/CommentShow/CommentInput.js
+++ React/hoc-code/src/pages/CommentShow/CommentInput.js
@@ -1,70 +1,91 @@
 /**
  *  CommentInput Input组件
  */
-import React, { Component } from 'react'
+import React, { Component } from "react";
 export default class CommentInput extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            content: ''
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      content: "",
+    };
+  }
+  // 监听name
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
 
-    handleUsernameChange(e){
-        this.setState({
-            username: e.target.value
-        })
-    }
+  // 监听content
+  handleContentChange(e) {
+    this.setState({
+      content: e.target.value,
+    });
+  }
 
-    handleContentChange(e) {
-        this.setState({
-            content: e.target.value
-        })
-    } 
-
-    handleSubmit() {
-        if(this.props.onSubmit) {
-            const {username, content} = this.state;
-            this.props.onSubmit({username, content});
-        }
-        this.setState({content:''});
+  // 点击提交
+  handleSubmit() {
+    if (this.props.onSubmit) {
+      const { username, content } = this.state;
+      this.props.onSubmit({ username, content });
     }
+    this.setState({ content: "" });
+  }
 
-    render() {
-        let {username, content} = this.state;
-        return (
-            <div className="comment-input">
-                <div className="comment-field">
-                    <span className="comment-field-name">
-                        用户名:
-                </span>
-                    <div className="comment-field-input">
-                        <input 
-                            value={username}
-                            onChange={this.handleUsernameChange.bind(this)}
-                        />
-                    </div>
-                </div>
-                <div className="comment-field" >
-                    <span className="comment-field-name" >
-                        评论内容：
-                </span>
-                    <div className="comment-field-input" >
-                        <textarea 
-                            value={content}
-                            onChange={this.handleContentChange.bind(this)}
-                        />
-                    </div>
-                    <div className='comment-field-button'>
-                        <button 
-                        onClick={this.handleSubmit.bind(this)}
-                        >
-                            发布
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
+  // 加载时候获取localStorage值
+  _loadUsername() {
+    const username = localStorage.getItem("username");
+    if (username) {
+      this.setState({ username });
     }
+  }
+  // 保存username到localStorage
+  _saveUsername(username) {
+    localStorage.setItem("username", username);
+  }
+
+  // 失去焦点执行保存方法
+  handleUsernameBlur(event) {
+    this._saveUsername(event.target.value);
+  }
+
+  UNSAFE_componentWillMount() {
+    this._loadUsername();
+  }
+
+  componentDidMount() {
+    this.textarea.focus();
+  }
+
+  render() {
+    let { username, content } = this.state;
+    return (
+      <div className="comment-input">
+        <div className="comment-field">
+          <span className="comment-field-name">用户名:</span>
+          <div className="comment-field-input">
+            <input
+              value={username}
+              onBlur={this.handleUsernameBlur.bind(this)}
+              onChange={this.handleUsernameChange.bind(this)}
+            />
+          </div>
+        </div>
+        <div className="comment-field">
+          <span className="comment-field-name">评论内容：</span>
+          <div className="comment-field-input">
+            <textarea
+              ref={(textarea) => (this.textarea = textarea)}
+              value={content}
+              onChange={this.handleContentChange.bind(this)}
+            />
+          </div>
+          <div className="comment-field-button">
+            <button onClick={this.handleSubmit.bind(this)}>发布</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
