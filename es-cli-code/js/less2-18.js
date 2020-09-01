3ac3831dfc1a5608f5fa9352b10708d3a2214fbb
xiaoguiking <king1129371217@163.com>
2020-09-01 23:10:51 +0800

    export import

---
+++ es-cli-code/js/less2-18.js
@@ -0,0 +1,26 @@
+// import { name, mod, list } from "./lesson2-18mod";
+
+// console.log(name, mod, list, "name");
+
+// import say, { say1, speak, run, running } from "./lesson2-18mod";
+// import obj from "./lesson2-18mod";
+
+// say("hello function ");
+
+// say1("hello say1");
+
+// speak();
+
+// run();
+
+// running();
+
+// let { data, error } = obj;
+
+// console.log(data, error);
+
+import Test from "./lesson2-18mod";
+
+let test = new Test();
+
+console.log(test.id, "number");
---
+++ es-cli-code/js/lesson2-18mod.js
@@ -0,0 +1,59 @@
+// export const name = "我是";
+// export let mod = "模块";
+// export let list = [1, 2, 4];
+
+/**
+ * 整体倒出
+ */
+
+// const name = "我是";
+// let mod = "模块";
+// const list = Array.from(Array(10).keys());
+
+// export { name, mod, list };
+
+/**
+ * 函数倒出
+ */
+
+// export default function say(word) {
+//   console.log(word);
+// }
+// export function say1(word) {
+//   console.log(word);
+// }
+
+// export function speak() {
+//   console.log("this is speaking");
+// }
+
+// const run = () => {
+//   console.log("this is run");
+// };
+
+// const running = () => {
+//   console.log("this is running");
+// };
+
+// export { run, running };
+
+/**
+ * 默认倒出obj
+ */
+// const data = {
+//   code: 1,
+//   message: "成功",
+// };
+
+// const error = {
+//   code: 0,
+//   messsage: "失败",
+// };
+
+// export default { data, error };
+
+export default class Test {
+  constructor() {
+    this.id = 4;
+  }
+}
