9d26544b3ea394bd257dfe0182cb6d388cacbef9
xiaoguiking <king1129371217@163.com>
2020-11-08 20:11:54 +0800

    dev-getter

---
+++ Vue/vue-vuex/src/components/Part1/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>我的信息: {{ myInfo }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyPart",
+  data() {
+    return {
+      myInfo: "无"
+    };
+  },
+  mounted() {
+    this.myInfo = this.$store.getters.getMyInfo;
+    console.log(this.myInfo);
+  }
+};
+</script>
+
+<style scoped></style>
